ef6692fcc33444dd620d77e5b378abdca984ac8b
Tail-19 <zhenghanlu2020@163.com>
2022-12-28 17:47:54 +0800

    Initial commit

---
+++ src/main/java/com/example/debateteamos/DebateTeamOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.debateteamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateTeamOsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebateTeamOsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/debateteamos/DebateTeamOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.debateteamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateTeamOsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
