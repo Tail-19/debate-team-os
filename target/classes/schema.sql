e64c67c0e0799510c6e780da929047679c06e81b
Tail-19 <zhenghanlu2020@163.com>
2023-01-01 18:27:29 +0800

    合并，增加登录注册之间的跳转按钮

---
+++ target/classes/schema.sql
@@ -0,0 +1,27 @@
+USE `test`;
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files`
+(
+    `id`            BIGINT(20) UNSIGNED AUTO_INCREMENT,
+    `name`          varchar(32) DEFAULT NULL,
+    `path`          varchar(64) DEFAULT NULL,
+    `fileType`      varchar(32),
+    `uploader`      varchar(32),
+    `uploaderTitle` varchar(32),
+    `tag`           varchar(32),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+DROP TABLE IF EXISTS `usertable`;
+CREATE TABLE `usertable`
+(
+    `id`         BIGINT(20) UNSIGNED AUTO_INCREMENT,
+    `username`   varchar(32) DEFAULT NULL,
+    `student_id` varchar(32) DEFAULT NULL,
+    `password`   varchar(64),
+    `phone`      varchar(32),
+    `email`      varchar(32),
+    `token`      varchar(128),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
